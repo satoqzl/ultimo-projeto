6b76193cbee633e050c152bb9144bef81d4cabd4
satoqzl <eduardosato2019@gmail.com>
2024-11-18 09:08:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,79 @@
+criaCartao(
+    'Futebol',
+    'Qual o maior time do Brasil?',
+    'Corinthians'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+
+criaCartao(
+    'futebol',
+    'qual jogador tem mais bola de ouro?',
+    'Lionel Messi'
+)
+
+
+criaCartao(
+    'futebol',
+    'qual o menor time do brasil?',
+    'santos'
+)
+
+
+criaCartao(
+    'futebol',
+    'qual time do brasil nao tem mundial?',
+    'palmeiras'
+)
+
+
+criaCartao(
+    'historia',
+    'quem descobriu o brasil?',
+    'Pedro alvares Cabral'
+)
+
+
+criaCartao(
+    'geografia',
+    'qual a capital do canada?',
+    'Ottawa'
+)
+
+
+criaCartao(
+    'futebol',
+    'quantos mundial o Corinthians tem?',
+    '2'
+)
+
+
+criaCartao(
+    'futebol',
+    'qual foi o jogador que ganhou 4 bola de ouro seguidas?',
+    'Lionel Messi'
+)
+
+
+criaCartao(
+    'futebol',
+    'quem e o jogador com o futebol mais lindo?',
+    'Neymar jr'
+)
